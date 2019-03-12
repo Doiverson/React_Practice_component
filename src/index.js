3b45d1c2c3ba122ee3aea12eafd8e094ece4caa5
Shosuke Doi <doiverson.canada@gmail.com>
2019-03-11 19:34:53 -0700

    done 40

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import faker from 'faker';
 import CommentDetail from './commentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
       <div className="ui container comments">
-          <CommentDetail />
+          <ApprovalCard>
+              <div>
+                  <h4>Warning</h4>
+                  Are you sure you want to this?
+              </div>
+          </ApprovalCard>
+
+          <ApprovalCard>
+              <CommentDetail
+                  author="Sam"
+                  timeAgo="Today at 4:45PM"
+                  content="Nice blog post"
+                  avatar={faker.image.avatar()}
+              />
+          </ApprovalCard>
+          <ApprovalCard>
+          <CommentDetail
+              author="Alex"
+              timeAgo="today at 2:00AM"
+              content="I like the subject"
+              avatar={faker.image.avatar()}
+          />
+          </ApprovalCard>
+          <ApprovalCard>
+          <CommentDetail
+              author="Jane"
+              timeAgo="Yesterday at 5:00PM"
+              content="I like the writing"
+              avatar={faker.image.avatar()}
+          />
+          </ApprovalCard>
       </div>
   );
 };
